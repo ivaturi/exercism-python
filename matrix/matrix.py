b0d4bbed055dba495647cbcaa2553fa46285a12a
abhi.ivaturi <abhi.ivaturi@kollmorgen.com>
2020-05-15 00:17:17 +0200

    Second attempt at solving matrix. Row extraction is still complicated

--- matrix/matrix.py
+++ matrix/matrix.py
@@ -1,10 +1,13 @@
 #! /usr/bin/env python
 class Matrix:
     def __init__(self, matrix_string):
-        self.rows = [x.split(" ") for x in matrix_string.split("\n")]
+        r, n = matrix_string.split("\n"), matrix_string.count("\n") + 1
+        self.rows = [list(map(int, r[i].split(" "))) for i in range(0, n)]
+        self.cols = list(map(list,zip(*self.rows)))
 
     def row(self, index):
-        return list(map(int, self.rows[index-1]))
+        return self.rows[index-1]
 
     def column(self, index):
-        return list(map(int, [row[index-1] for row in self.rows]))
+        return self.cols[index-1]
+
