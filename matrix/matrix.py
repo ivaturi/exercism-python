5e0c26e23a46269397956b63e3a68926fff4ede5
abhi.ivaturi <abhi.ivaturi@kollmorgen.com>
2020-05-15 00:17:45 +0200

    Third attempt at solving matrix. Row extraction is still complicated

--- matrix/matrix.py
+++ matrix/matrix.py
@@ -1,8 +1,13 @@
 #! /usr/bin/env python
 class Matrix:
     def __init__(self, matrix_string):
+        
         r, n = matrix_string.split("\n"), matrix_string.count("\n") + 1
+        
+        # can this be simplified?
         self.rows = [list(map(int, r[i].split(" "))) for i in range(0, n)]
+        
+        # list(zip) doesn't work because it outputs sets, and we're expecting arrays
         self.cols = list(map(list,zip(*self.rows)))
 
     def row(self, index):
