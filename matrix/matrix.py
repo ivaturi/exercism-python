7fd29d020878c994bee3e7505bd65e30f27b0f5a
abhi.ivaturi <abhi.ivaturi@kollmorgen.com>
2020-05-17 00:00:29 +0200

    Final solution for matrix

--- matrix/matrix.py
+++ matrix/matrix.py
@@ -1,11 +1,9 @@
 #! /usr/bin/env python
 class Matrix:
     def __init__(self, matrix_string):
-        
-        r, n = matrix_string.split("\n"), matrix_string.count("\n") + 1
-        
+       
         # can this be simplified?
-        self.rows = [list(map(int, r[i].split(" "))) for i in range(0, n)]
+        self.rows = [list(map(int, s.split(" "))) for s in matrix_string.split("\n")]
         
         # list(zip) doesn't work because it outputs sets, and we're expecting arrays
         self.cols = list(map(list,zip(*self.rows)))
