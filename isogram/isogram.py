1e1c6c267d41844ea99ac263a47f00f3a1e5657a
abhi.ivaturi <abhi.ivaturi@kollmorgen.com>
2020-09-18 06:50:21 +0200

    isogram

--- isogram/isogram.py
+++ isogram/isogram.py
@@ -1,2 +1,7 @@
 def is_isogram(string):
-    pass
+
+    # brute force
+    c = 'abcdefghijklmnopqrstuvwxyz'
+    s = string.lower()
+    check = sum([s.count(l) > 1 for l in s if l in c]) < 1
+    return bool(check)
